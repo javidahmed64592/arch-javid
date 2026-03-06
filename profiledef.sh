afeae31a62709c090069d712d05abd29630916c3
Javid Ahmed <javidahmed@icloud.com>
2026-03-06 21:14:51 +0000

    Update profiledef.sh: simplify date commands and correct publisher information

--- profiledef.sh
+++ profiledef.sh
@@ -2,18 +2,16 @@
 # shellcheck disable=SC2034
 
 iso_name="arch-javid"
-iso_label="ARCH_JAVID_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Javid <https://github.com/javid>"
+iso_label="ARCH_JAVID_$(date +%Y%m)"
+iso_publisher="Javid Ahmed <https://github.com/javidahmed64592>"
 iso_application="Arch Linux Live/Rescue CD"
-iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
+iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
-buildmodes=('iso')
 bootmodes=('uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
-bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/etc/gshadow"]="0:0:400"
